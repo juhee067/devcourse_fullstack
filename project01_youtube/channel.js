8c7f4853708a22ae05cc2570b34d2285ba92c485
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-03-25 22:09:57 +0900

    feat: 로그인 고도화 및 채널 생성

---
+++ project01_youtube/channel.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const app = express();
+app.listen(3001);
+app.use(express.json());
+
+let db = new Map();
+let id = 1;
+
+// "/channels"
+app
+  .route('/channels')
+  // 채널 전체 조회
+  .get((req, res) => {
+    const channels = Array.from(db.values());
+    if (channels.length > 0) {
+      return res.status(200).json({
+        status: 200,
+        channelList: channels,
+      });
+    }
+    res.status(404).json({
+      status: 404,
+      msg: '채널을 찾을 수 없습니다.',
+    });
+  })
+  // 채널 개별 생성
+  .post((req, res) => {
+    const channelData = req.body;
+    if (channelData.channelTitle) {
+      db.set(id++, channelData);
+      return res.status(201).json({
+        status: 201,
+        msg: `${channelData.channelTitle} 채널 생성을 성공적으로 완료하였습니다.`,
+      });
+    }
+    res.status(400).json({ status: 400, msg: '입력한 데이터가 없습니다.' });
+  });
+
+// "/channels/:id"
+app
+  .route('/channels/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const channel = db.get(parseInt(id));
+    if (!channel) {
+      return res.status(404).json({
+        status: 404,
+        msg: `채널을 찾을 수 없습니다.`,
+      });
+    }
+    res.status(200).json({
+      status: 200,
+      msg: channel.channelTitle,
+    });
+  })
+  .put((req, res) => {
+    let { id } = req.params;
+    id = parseInt(id);
+    const channel = db.get(id);
+    if (channel) {
+      const newTitle = req.body.channelTitle;
+      const preChannel = channel.channelTitle;
+      channel.channelTitle = newTitle;
+      db.set(id, channel);
+      return res.status(201).json({
+        status: 201,
+        msg: `${preChannel} 채널명이 ${newTitle} 채널 명으로 성공적으로 수정됐습니다.`,
+      });
+    }
+    res.status(404).json({ status: 404, msg: '해당 채널을 찾을 수 없습니다.' });
+  })
+  .delete((req, res) => {
+    let { id } = req.params;
+    id = parseInt(id);
+    const channel = db.get(id);
+    if (channel) {
+      db.delete(id);
+      return res.status(200).json({
+        status: 200,
+        msg: `${channel.channelTitle} 채널명이 성공적으로 삭제됐습니다.`,
+      });
+    }
+    res.status(404).json({ status: 404, msg: '해당 채널을 찾을 수 없습니다.' });
+  });
--- project01_youtube/user.js
+++ project01_youtube/user.js
@@ -1,28 +1,51 @@
 const express = require('express');
 const app = express();
-app.listen(3000);
+app.listen(3001);
 app.use(express.json());
 
 // db 설정
 let db = new Map();
 let id = 1;
 
+const isEmptyObject = (obj) => {
+  return Object.keys(obj).length !== 0;
+};
+
 // 로그인
+// forEach
 app.post('/signin', (req, res) => {
-  const userData = req.body;
-  res.status(200).json({ status: '200', msg: `${req.body.userId}님 로그인 성공` });
+  // userId가 디비에 저장된 회원인지 확인
+  const { userId, password } = req.body;
+
+  let userData = {};
+  db.forEach((user, id) => {
+    if (user.userId == userId) {
+      userData = user;
+    }
+  });
+
+  // userID를 못찾았으면
+  // if (userData.userId) {
+  if (isEmptyObject(userData)) {
+    if (userData.password == password) {
+      return res.status(200).json({ status: 200, msg: `${userId}님 로그인 성공` });
+    }
+    res.status(400).json({ status: 400, msg: `비밀번호가 맞지않습니다.` });
+  } else {
+    res.status(400).json({ status: 400, msg: `${userId}, 입력하신 아이디는 없는 아이디입니다.` });
+  }
 });
 
 //  회원가입
 app.post('/signup', (req, res) => {
   const userData = req.body;
 
-  if (userData.name) {
-    return res.status(201).json({ status: '200', msg: `${userData.name}님 회원가입 성공` });
+  if (userData) {
+    db.set(id++, userData);
+    return res.status(201).json({ status: 200, msg: `${userData.name}님 회원가입 성공` });
   }
 
-  db.set(id++, userData);
-  res.status(400).json({ status: '400', msg: '데이터를 입력해주세요' });
+  res.status(400).json({ status: 400, msg: '데이터를 입력해주세요' });
 });
 
 //  회원 개별 조회
@@ -30,9 +53,9 @@ app.get('/users/:id', (req, res) => {
   const { id } = req.params;
   const user = db.get(parseInt(id));
   if (user) {
-    return res.status(200).json({ status: '200', userId: user.userId, name: user.name });
+    return res.status(200).json({ status: 200, userId: user.userId, name: user.name });
   }
-  res.status(404).json({ status: '400', msg: '해당 유저가 존재하지 않습니다.' });
+  res.status(404).json({ status: 400, msg: '해당 유저가 존재하지 않습니다.' });
 });
 
 //  회원 개별 탈퇴
@@ -42,30 +65,8 @@ app.delete('/users/:id', (req, res) => {
 
   if (user) {
     db.delete(parseInt(id));
-    return res.status(200).json({ status: '200', msg: `${user.name}님, 성공적으로 탈퇴가 됐습니다.` });
+    return res.status(200).json({ status: 200, msg: `${user.name}님, 성공적으로 탈퇴가 됐습니다.` });
   }
 
-  res.status(400).json({ status: '400', msg: '해당 유저가 존재하지 않습니다.' });
+  res.status(400).json({ status: 400, msg: '해당 유저가 존재하지 않습니다.' });
 });
-
-app
-  .route('/users/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    const user = db.get(parseInt(id));
-    if (user) {
-      return res.status(200).json({ status: '200', userId: user.userId, name: user.name });
-    }
-    res.status(404).json({ status: '400', msg: '해당 유저가 존재하지 않습니다.' });
-  })
-  .delete((req, res) => {
-    const { id } = req.params;
-    const user = db.get(parseInt(id));
-
-    if (user) {
-      db.delete(parseInt(id));
-      return res.status(200).json({ status: '200', msg: `${user.name}님, 성공적으로 탈퇴가 됐습니다.` });
-    }
-
-    res.status(400).json({ status: '400', msg: '해당 유저가 존재하지 않습니다.' });
-  });
