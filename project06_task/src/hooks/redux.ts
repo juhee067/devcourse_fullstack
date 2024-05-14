4958629469a6038ebd358097ae598daabf6d4c1d
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-14 12:10:20 +0900

    feat: redux hooks 생성

---
+++ project06_task/src/hooks/redux.ts
@@ -0,0 +1,7 @@
+import { RootState } from '@reduxjs/toolkit/query';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../store';
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
+const logger = useTypedSelector((state) => state.logger);
--- project06_task/src/store/index.ts
+++ project06_task/src/store/index.ts
@@ -2,7 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer/reducer';
 
 const store = configureStore({
-  reducer: reducer,
+  reducer,
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+const logger = useSelector((state: RootState) => state.logger);
 
 export default store;
