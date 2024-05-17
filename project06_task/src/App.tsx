b276a74c7d08e24e7305549fb3fbc2b6f5fefaf9
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-17 14:18:09 +0900

    feat:task modal popup

--- project06_task/src/App.tsx
+++ project06_task/src/App.tsx
@@ -4,15 +4,18 @@ import BoardList from './components/BoardList/BoardList';
 import ListsContainer from './components/ListContainer/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
 import { IBoardItem } from './store/types';
+import EditModal from './components/EditModal/EditModal';
 
 function App() {
   const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
   const boards = useTypedSelector((state) => state.boards.boardArray);
 
   const getActiveBoard = boards.filter((board: IBoardItem) => board.boardId === activeBoardId)[0];
   const lists = getActiveBoard.lists;
   return (
     <div className={appContainer}>
+      {modalActive && <EditModal />}
       <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId} />
       <div className={board}>
         <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
---
+++ project06_task/src/components/EditModal/EditModal.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { FiX } from 'react-icons/fi';
+import { useTypedDispatch, useTypedSelector } from '../../hooks/redux';
+import { setModalActive } from '../../store/slices/boardSlice';
+
+const EditModal = () => {
+  const dispatch = useTypedDispatch();
+  const editingState = useTypedSelector((state) => state.modal);
+  const [data, setData] = useState(editingState);
+  const handleCloseButton = () => {
+    dispatch(setModalActive(false));
+  };
+  return (
+    <div>
+      <div>
+        <div>
+          <div>{editingState.task.taskName}</div> <FiX onClick={handleCloseButton} />
+        </div>
+        <div>제목</div>
+        <input type='text' value={data.task.taskName} />
+        <div>설명</div>
+        <input type='text' value={data.task.taskDescription} />
+        <div>생성한 사람</div>
+        <input type='text' value={data.task.taskOwner} />
+      </div>
+      <div>
+        <button>수정하기</button>
+        <button>삭제하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default EditModal;
