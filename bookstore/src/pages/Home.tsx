3461185d827a1ed44fcb8f0af15d282e85955d23
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-05-29 21:36:31 +0900

    feat:폴더 생성

---
+++ bookstore/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from '../components/common/Header';
+import { formatNumber } from '../utils/format';
+
+const COUNT = 10000;
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div>Home</div>
+      <div>count : {formatNumber(COUNT)}</div>
+    </>
+  );
+};
+
+export default Home;
