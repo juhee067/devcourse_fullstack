e053511764c785ff2c884e6cfc0ec966443ab786
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-02 19:30:46 +0900

    feat:error page 생성

--- bookstore/src/App.tsx
+++ bookstore/src/App.tsx
@@ -2,12 +2,21 @@ import Layout from './layout/Layout';
 import Home from './pages/Home';
 
 import { BookStoreThemeProvider } from './context/themeContext';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Error from './components/common/Error';
+const router = createBrowserRouter([
+  { path: '/', element: <Home />, errorElement: <Error /> },
+  {
+    path: '/books',
+    element: <div>도서목록</div>,
+  },
+]);
 
 function App() {
   return (
     <BookStoreThemeProvider>
       <Layout>
-        <Home />
+        <RouterProvider router={router} />
       </Layout>
     </BookStoreThemeProvider>
   );
