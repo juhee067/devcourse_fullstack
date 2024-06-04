8f3904aabea0edfd42a9f1466b98f1fe04c5dc7b
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-04 22:25:27 +0900

    feat:현재 카테고리 표시

--- bookstore/src/components/books/BooksFilter.tsx
+++ bookstore/src/components/books/BooksFilter.tsx
@@ -1,16 +1,38 @@
 import styled from 'styled-components';
 import { useCategory } from '../../hooks/useCategory';
 import Button from '../common/Button';
+import { useSearchParams } from 'react-router-dom';
 
 const BooksFilter = () => {
   const { category } = useCategory();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleCategory = (id: number | null) => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    if (id === null) {
+      newSearchParams.delete('category_id');
+    } else {
+      newSearchParams.set('category_id', id.toString());
+    }
+
+    setSearchParams(newSearchParams);
+  };
+
+  const currentCategory = searchParams.get('category_id');
+
   return (
     <BooksFilterStyle>
       <div className='category'>
         {category.map((item) => (
-          <Button size={'medium'} scheme={'normal'}>
-            {item.category_name}
-          </Button>
+          <div key={item.id} onClick={() => handleCategory(item.id)}>
+            <Button
+              size={'medium'}
+              scheme={currentCategory === item.id?.toString() ? 'primary' : 'normal'}
+            >
+              {item.category_name}
+            </Button>
+          </div>
         ))}
       </div>
       <div className='new'>
