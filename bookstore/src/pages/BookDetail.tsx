d8cdfe4b245e1e48d91d281243596ebd6df7b4a6
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-05 12:59:41 +0900

    feat: bookdetail 상세 정보 보여주기

--- bookstore/src/pages/BookDetail.tsx
+++ bookstore/src/pages/BookDetail.tsx
@@ -1,11 +1,58 @@
 import { useParams } from 'react-router-dom';
 import { useBook } from '../hooks/useBook';
+import { getImgSrc } from '../utils/image';
+import { Title } from '../components/common/Title';
+import { BookDetail as IBookDetail } from '../models/book.model';
+import styled from 'styled-components';
+import { formatDate, formatNumber } from '../utils/format';
+const bookInfoList = [
+  { label: '카테고리', key: 'categoryName' },
+  { label: '포맷', key: 'form' },
+  { label: '페이지', key: 'pages' },
+  { label: 'ISBN', key: 'isbn' },
+  {
+    label: '출간일',
+    key: 'pubDate',
+    filter: (book: IBookDetail) => {
+      return `${formatDate(book.pubDate)} 원`;
+    },
+  },
+  {
+    label: '가격',
+    key: 'price',
+    filter: (book: IBookDetail) => {
+      return `${formatNumber(book.price)} 원`;
+    },
+  },
+];
 
 const BookDetail = () => {
   const { bookId } = useParams();
   const { book } = useBook(bookId);
   if (!book) return null;
-  return <div>{book.title}</div>;
+  return (
+    <BookDetailStyle>
+      <header className='header'>
+        <div className='img'>
+          <img src={getImgSrc(book.img)} alt={book.title} />
+        </div>
+        <div className='info'>
+          <Title size='large' color='text'>
+            {book.title}
+          </Title>
+          {bookInfoList.map((item) => (
+            <dl key={item.key}>
+              <dt>{item.label}</dt>
+              <dd>{item.filter ? item.filter(book) : book[item.key as keyof IBookDetail]}</dd>
+            </dl>
+          ))}
+        </div>
+      </header>
+      <div className='content'> </div>
+    </BookDetailStyle>
+  );
 };
+
+const BookDetailStyle = styled.div``;
 
 export default BookDetail;
--- bookstore/src/utils/format.ts
+++ bookstore/src/utils/format.ts
@@ -1,3 +1,7 @@
 export const formatNumber = (num: number) => {
   return num.toLocaleString();
 };
+
+export const formatDate = (date: string) => {
+  return date;
+};
