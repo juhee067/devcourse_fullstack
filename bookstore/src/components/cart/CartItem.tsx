d379c849d025b00ae8bc0800e18dc4ae855c0acd
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-08 21:13:03 +0900

    style:cartItem style 생성

--- bookstore/src/components/cart/CartItem.tsx
+++ bookstore/src/components/cart/CartItem.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
 import styled from 'styled-components';
+import { Title } from '../components/common/Title'; // 필요한 컴포넌트를 임포트
+import { formatNumber } from '../utils/format'; // 필요한 유틸리티 함수를 임포트
+import Button from '../common/Button';
 
-const CartItem = () => {
+interface CartItemProps {
+  cart: {
+    title: string;
+    summary: string;
+    price: number;
+    quantity: number;
+  };
+}
+
+function CartItem({ cart }: CartItemProps) {
   return (
-    <>
-      <CartItemStyle></CartItemStyle>
-    </>
+    <CartItemStyle>
+      <div className='info'>
+        <div>체크 버튼</div>
+        <div>
+          <Title size='medium' color='text'>
+            {cart.title}
+          </Title>
+          <p className='summary'>{cart.summary}</p>
+          <p className='price'>{formatNumber(cart.price)} 원</p>
+          <p className='quantity'>{cart.quantity} 권</p>
+        </div>
+      </div>
+      <Button size='medium' scheme='normal'>
+        장바구니 삭제
+      </Button>
+    </CartItemStyle>
   );
-};
+}
+
+const CartItemStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  border: 1px solid ${({ theme }) => theme.colors.border};
+  border-radius: ${({ theme }) => theme.borderRadius.default};
+  padding: 12px;
+  p {
+    padding: 0 0 8px 0;
+    margin: 0;
+  }
+`;
 
 export default CartItem;
-
-const CartItemStyle = styled.div``;
--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
 import styled from 'styled-components';
 import { Title } from '../components/common/Title';
 import CartItem from '../components/cart/CartItem';
+import { useCart } from '../hooks/useCart';
 
 const Cart = () => {
+  const { carts } = useCart();
+  console.log(carts);
   return (
     <>
       <Title size={'large'}>장바구니</Title>{' '}
       <CartStyle>
         <div className='content'>
-          <CartItem />
+          {carts.map((item) => (
+            <CartItem key={item.id} cart={item} />
+          ))}
         </div>
         <div className='summary'></div>
       </CartStyle>
