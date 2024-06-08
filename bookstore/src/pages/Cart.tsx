f557c4c17134dbcb55540db7bb9ce85092b92cfa
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-08 22:16:17 +0900

    style:cart style 지정

--- bookstore/src/pages/Cart.tsx
+++ bookstore/src/pages/Cart.tsx
@@ -3,9 +3,11 @@ import { Title } from '../components/common/Title';
 import CartItem from '../components/cart/CartItem';
 import { useCart } from '../hooks/useCart';
 import { useState } from 'react';
+import Empty from '../components/common/Empty';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Cart = () => {
-  const { carts, deleteCartItem } = useCart();
+  const { carts, deleteCartItem, isEmpty } = useCart();
   const [checkedItems, setCheckedItems] = useState<number[]>([]);
 
   const handleCheckItem = (id: number) => {
@@ -25,18 +27,28 @@ const Cart = () => {
     <>
       <Title size={'large'}>장바구니</Title>{' '}
       <CartStyle>
-        <div className='content'>
-          {carts.map((item) => (
-            <CartItem
-              key={item.id}
-              cart={item}
-              checkedItems={checkedItems}
-              onCheck={handleCheckItem}
-              onDelete={handleItemDelete}
-            />
-          ))}
-        </div>
-        <div className='summary'></div>
+        {!isEmpty ? (
+          <>
+            <div className='content'>
+              {carts.map((item) => (
+                <CartItem
+                  key={item.id}
+                  cart={item}
+                  checkedItems={checkedItems}
+                  onCheck={handleCheckItem}
+                  onDelete={handleItemDelete}
+                />
+              ))}
+            </div>
+            <div className='summary'></div>
+          </>
+        ) : (
+          <Empty
+            title='장바구니가 비었습니다'
+            icon={<FaShoppingCart />}
+            description={<>장바구니를 채워보세요</>}
+          />
+        )}
       </CartStyle>
     </>
   );
@@ -44,4 +56,20 @@ const Cart = () => {
 
 export default Cart;
 
-const CartStyle = styled.div``;
+const CartStyle = styled.div`
+  display: flex;
+  gap: 24px;
+  justify-content: space-between;
+  padding: 24px 0 0 0;
+
+  .content {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+  }
+
+  .summary {
+    display: flex;
+  }
+`;
