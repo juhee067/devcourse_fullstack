059e88ea0c81683196c39c820ec970e78fbe2c0d
juhee067 <66045537+juhee067@users.noreply.github.com>
2024-06-10 12:21:34 +0900

    feat:msw 세팅

--- bookstore/src/main.tsx
+++ bookstore/src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mock/browser');
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
